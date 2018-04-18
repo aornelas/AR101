0d0c1d7239bc32daadf2c505f429bcbc2db23ee9
Andres Ornelas <andres@twitter.com>
2018-04-18 06:41:32 -0700

    Output rocket name to log

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RocketController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   private string _rocketName;
+
+   public void Start () {
+      _rocketName = gameObject.name;
+      Debug.Log("Rocket name: " + _rocketName);
+   }
+
+   // Update is called once per frame
+   void Update () {
+
+   }
 }
